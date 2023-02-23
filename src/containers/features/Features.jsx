08eb4d0cb3182f4702a2747426d56366775818a2
Joao Oliveira <jaoliveira98@gmail.com>
2023-02-23 13:36:38 +0000

    Features component styling

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -23,15 +23,15 @@ const data = [
 
 function Features() {
   return (
-    <div id="features">
-      <div>
-        <h1>
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
           The Future is Now and You Just Need to Realize It. Step into Future
           Today. & Make it Happen.
         </h1>
         <p>Request Early Access to Get Started</p>
       </div>
-      <div>
+      <div className="gpt3__features-container">
         {data.map((item, index) => (
           <Feature
             title={item.title}
