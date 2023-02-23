3a932d64a345eb8a9026ee731965724380e92048
Joao Oliveira <jaoliveira98@gmail.com>
2023-02-23 11:28:15 +0000

    WhatGPT3 content mapping

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import "./whatgpt3.css";
 import Feature from "../../components/feature/Feature";
 
+const data = [
+  {
+    title: "Chatbots",
+    text: "Chatbots are computer programs designed to simulate conversation with human users, often used in customer service and online support.",
+  },
+  {
+    title: "Knowledgebase",
+    text: "A centralized repository of information and data that can be accessed and shared across an organization, allowing for efficient information management and sharing.",
+  },
+  {
+    title: "Education",
+    text: "The process of acquiring knowledge through formal instruction or practical experience, aimed at developing the intellectual and personal abilities.",
+  },
+];
+
 function WhatGPT3() {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature
           title="What is GPT-3"
-          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+          text="Hey there, have you heard about GPT3 AI? It's like having your very own personal assistant, except it never sleeps, gets tired, or complains about how much work you're giving it! This bad boy can handle everything from writing essays to coding programs to generating memes - seriously, it's like having a Swiss Army knife for your brain. So if you want to boost your productivity and have some fun while you're at it, give GPT3 AI a try. It might just become your new best friend (sorry, human friends!)."
         />
       </div>
       <div className="gpt3__whatgpt3-heading">
@@ -18,18 +33,9 @@ function WhatGPT3() {
         <p>Explore the Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature
-          title="Chatbots"
-          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
-        />
-        <Feature
-          title="Knowledgebase"
-          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
-        />
-        <Feature
-          title="Education"
-          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
-        />
+        {data.map((item, index) => (
+          <Feature key={index} title={item.title} text={item.text} />
+        ))}
       </div>
     </div>
   );
