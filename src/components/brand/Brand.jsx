857e93ad7cec4b1c13127347e06b17d320e6a7cd
Joao Oliveira <jaoliveira98@gmail.com>
2023-02-23 09:47:29 +0000

    Brands Component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./brand.css";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import google from "../../assets/google.png";
+import shopify from "../../assets/shopify.png";
+import slack from "../../assets/slack.png";
 
 function Brand() {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+    </div>
+  );
 }
 
 export default Brand;
