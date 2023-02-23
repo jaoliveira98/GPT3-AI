7171b0fab578398d298e811eca66b6e93a3266f9
Joao Oliveira <jaoliveira98@gmail.com>
2023-02-23 15:58:42 +0000

    CSS container queries

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ function App() {
   return (
     <div className="App">
       <div className="gradient__bg">
-        <Navbar />
-        <Header />
+        <div className="container">
+          <Navbar />
+          <Header />
+        </div>
       </div>
-      <div>
+      <div className="container">
         <Brand />
         <WhatGPT3 />
         <Features />
